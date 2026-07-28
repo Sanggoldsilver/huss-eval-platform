9116846e6a0c7dc5dcb275011a95967dbbd43fdc
이상은 <202110123@sangmyung.kr>
2026-07-28 22:30:07 +0900

    fix: Correct @googleapis/drive import types for zero TypeScript errors

--- src/lib/driveServer.ts
+++ src/lib/driveServer.ts
@@ -1,16 +1,16 @@
-import { google } from '@googleapis/drive';
+import { drive, auth } from '@googleapis/drive';
 
 /**
  * Google Drive API를 통한 파일 업로드
  * (서버 환경 전용)
  */
 export async function uploadToGoogleDrive(fileName: string, mimeType: string, fileStream: any) {
-  const auth = new google.auth.GoogleAuth({
+  const googleAuth = new auth.GoogleAuth({
     scopes: ['https://www.googleapis.com/auth/drive.file'],
   });
-  const drive = google.drive({ version: 'v3', auth });
+  const driveClient = drive({ version: 'v3', auth: googleAuth });
 
-  const response = await drive.files.create({
+  const response = await driveClient.files.create({
     requestBody: {
       name: fileName,
       mimeType: mimeType,
@@ -29,12 +29,12 @@ export async function uploadToGoogleDrive(fileName: string, mimeType: string, fi
  * (서버 환경 전용)
  */
 export async function getDriveFileLink(fileId: string) {
-  const auth = new google.auth.GoogleAuth({
+  const googleAuth = new auth.GoogleAuth({
     scopes: ['https://www.googleapis.com/auth/drive.readonly'],
   });
-  const drive = google.drive({ version: 'v3', auth });
+  const driveClient = drive({ version: 'v3', auth: googleAuth });
 
-  const response = await drive.files.get({
+  const response = await driveClient.files.get({
     fileId: fileId,
     fields: 'webViewLink, webContentLink',
   });
